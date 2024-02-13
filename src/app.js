30648652baf8d8e540f0d5683599e4a2b656b4de
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-02-13 19:06:34 +0000

    Excuse generator with btn

--- src/app.js
+++ src/app.js
@@ -1,32 +1,34 @@
-document.getElementById("excuse").addEventListener("click");
-
 window.onload = function() {
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  document.getElementById("button").addEventListener("click", function() {
+    document.getElementById("excuse").innerHTML = randomGenerator;
 
-  //write your code here
-  let randomWho = Math.floor(Math.random() * who.length);
-  let randomAction = Math.floor(Math.random() * action.length);
-  let randomWhat = Math.floor(Math.random() * what.length);
-  let randomWhen = Math.floor(Math.random() * when.length);
-  let randomGenerator =
-    who[randomWho] +
-    " " +
-    action[randomAction] +
-    " " +
-    what[randomWhat] +
-    " " +
-    when[randomWhen];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
 
-  document.getElementById("excuse").innerHTML = randomGenerator;
+    //write your code here
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
+    let randomGenerator =
+      who[randomWho] +
+      " " +
+      action[randomAction] +
+      " " +
+      what[randomWhat] +
+      " " +
+      when[randomWhen];
+
+    document.getElementById("excuse").innerHTML = randomGenerator;
+  });
 };
 
 // Whenever the document is ready call the function
