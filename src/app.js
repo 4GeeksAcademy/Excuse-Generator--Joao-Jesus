baab5c94c52ed9febbe19f22850ccb6580cd7a39
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-02-13 18:31:18 +0000

    Excuse generator Joao Jesus final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+document.getElementById("excuse").addEventListener("click");
 
 window.onload = function() {
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+  let randomGenerator =
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen];
+
+  document.getElementById("excuse").innerHTML = randomGenerator;
 };
+
+// Whenever the document is ready call the function
+
+document.onload;
